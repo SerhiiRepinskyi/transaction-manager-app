2e613b30fb6a7ada6a2392d50dd7117ab2fe6455
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-06-29 17:08:35 +0300

    feat: added script for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from './components/App.tsx'
-import './index.css'
+import App from "./components/App.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/transaction-manager-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>
