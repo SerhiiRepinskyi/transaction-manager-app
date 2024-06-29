374a9450dc5c38e78b93b232fbbbdba05ebbeda4
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-06-29 17:09:49 +0300

    Updates

---
+++ assets/HomePage-BXNqRi4J.js
@@ -0,0 +1,12 @@
+import{p as i,j as e}from"./index-BnuTjNaQ.js";const p=i.div`
+  max-width: 1200px;
+  display: flex;
+  gap: 20px;
+
+  margin: 0 auto;
+  margin-top: 70px;
+  padding: 20px 8px 40px;
+
+  outline: 2px solid red;
+  // outline - рамка для обведення контенту
+`,n=()=>e.jsx("section",{children:e.jsx(p,{children:e.jsx("h2",{children:"==HomePage=="})})});export{n as default};
---
+++ assets/TransactionPage-BPFEE24x.js
@@ -0,0 +1,9 @@
+import{p as t,j as n}from"./index-BnuTjNaQ.js";const i=t.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  margin-top: 70px;
+  padding: 150px 8px 150px;
+
+  outline: 2px solid red;
+  // outline - рамка для обведення контенту
+`,o=()=>n.jsx("section",{children:n.jsx(i,{children:n.jsx("h2",{children:"==Transaction Page=="})})});export{o as default};
