013d957f7fa2eff74cf99caf03654f77d84f3d9a
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-07-01 09:34:55 +0300

    Updates

---
+++ assets/HomePage-CGqBmQgv.js
@@ -0,0 +1,17 @@
+import{p as e,j as t}from"./index-blIYtA84.js";const n=e.div`
+  max-width: 1200px;
+  height: 400px;
+  display: flex;
+  justify-content: center;
+  // align-items: center;
+
+  margin: 0 auto;
+  margin-top: 70px;
+  padding: 180px 8px 40px;
+
+  font-size: 24px;
+  font-weight: 600;
+
+  // outline: 2px solid red;
+  // outline - рамка для обведення контенту
+`,o=()=>t.jsx("section",{children:t.jsx(n,{children:t.jsx("h2",{children:"Transaction Manager App"})})});export{o as default};
